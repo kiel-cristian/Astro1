b00929389a0b9aef70d9aaf6e6505ef2a5408137
hsalas <hector.salas.o@gmail.com>
2012-09-13 19:03:49 -0300

    Update metodos_t1.py
    falta terminar el addgalaxy()..., el resto en teoria deberia funcionar

--- metodos_t1.py
+++ metodos_t1.py
@@ -11,6 +11,11 @@ def mToCounts (m, m0, F0):
     counts=exptime*F0*10**(-2/5*(m-m0))
 	return (counts)
 
+def RADECtoRowCol(RA,DEC):
+	row =(cd2_1*(crval1-RA)-cd1_1*(crval2-DEC))/(cd1_2*cd2_1-cd1_1*cd2_2)+crpix2
+	col =(-cd2_2*(crval1-RA)+cd1_2*(crval2-DEC))/(cd1_2*cd2_1-cd1_1*cd2_2)+crpix1
+	return (row,col)
+
 def addStar (hdu, m, RA, DEC):
     (ROW,COL)=RADECtoRowCol(RA,DEC)
 	if 0<=ROW<=4096 and 0<=COL<=4096:	
@@ -29,15 +34,35 @@ def addStellarCatalog (hdu, catalog):
 		addStar(hdu,mag,ra,dec)
 	return
 
+def E(xc,yc,x,y,el,theta)
+	e=math.sqrt(((x-xc)*math.cos(theta)+(y-yc)*math.sin(theta))**2+((x-xc)*math.sin(theta)-(y-yc)*math.cos(theta))**2/(1-el)**2)
+	return (e)
+
+def psersic (e,Re,n,i0,m)
+	bn=2*n-0.324
+	ln=mToCounts(m,20,flux20)
+	I0=ln*bn**2/((Re**2)*2math.pi*n*math.gamma(2*n))
+	I=I0*math.exp(-bn*(e/RE)**(1/n))
+	return (I)
+	
 def addGalaxy (hdu, m, RA, DEC, n, Re, el, theta):
-    # Definir rutina que agrega una galaxia de magnitud m en la
+	(ROW,COL)=RADECtoRowCol(RA,DEC)
     # coordenada RA, DEC, representada por un perfil de Sersic
-    # definido por n, Re, el y rotado en un �ngulo theta.
-    return
+    # definido por n, Re, el y rotado en un ángulo theta.
 
 def addGalaxyCatalog (hdu, catalog):
-    # Definir rutina que agregue un catalogo de galaxias a hdu.
-    return
+	for linea in open(catalog):
+		linea = linea.strip()
+		obj, ra, dec,mag,sed,redshift,tipo,n,re,el,theta = linea.split()
+		ra=float(ra)
+		dec=float(dec)
+		mag=float(mag)
+		n=float(n)
+		re=float(re)
+		el=float(el)
+		theta=float(theta)
+		addGalaxy(hdu,mag,ra,dec,n,re.el,theta)
+	return
 
 def addBackground (hdu, background):
     # Agrega un background constante a hdu.
