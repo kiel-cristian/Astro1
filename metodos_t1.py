3d970bfa372fe77eb9d81c03daf47fb6b903ec82
Cristian <the.killer06@gmail.com>
2012-09-14 02:20:16 -0300

    sacando arrays innecesarios

--- metodos_t1.py
+++ metodos_t1.py
@@ -24,18 +24,25 @@ cd2_1=hdulist[0].header['CD2_1']
 cd2_2=hdulist[0].header['CD2_2']
 
 def RADECtoRowCol(RA,DEC):
-	row =1/(cd1_2*cd2_1-cd1_1*cd2_2)*(cd2_1*(RA-crval1)-cd1_1*(DEC-crval2))+crpix2
-	col =1/(cd1_2*cd2_1-cd1_1*cd2_2)*(-cd2_2*(RA-crval1)+cd1_2*(DEC-crval2))+crpix1
+	row = 1/(cd1_2*cd2_1-cd1_1*cd2_2)*(cd2_1*(RA-crval1)-cd1_1*(DEC-crval2))+crpix2
+	col = 1/(cd1_2*cd2_1-cd1_1*cd2_2)*(-cd2_2*(RA-crval1)+cd1_2*(DEC-crval2))+crpix1
 	return (int(row),int(col))#revisar bien esto despues
 
+def psersic(Re,n,m,xc,yc,x,y,el,theta):
+	bn = 2*n-0.324
+	ln = mToCounts(m,20,flux20)
+	I0 = ln*bn**2/((Re**2)*2*pi*n*gamma(2*n))
+	E = sqrt(((x-xc)*cos(theta)+(y-yc)*sin(theta))**2+((x-xc)*sin(theta)-(y-yc)*cos(theta))**2/(1-el)**2)
+	I = I0*exp(-bn*(E/Re)**(1/n))
+	return I
+
 def mToCounts(m, m0, F0):
-	counts=exptime*F0*10**(-2/5*(m-m0))
-	return (counts)
+	return exptime*F0*10**(-2/5*(m-m0))
 
 def addStar(hdu, m, RA, DEC):
-	(ROW,COL)=RADECtoRowCol(RA,DEC)
-	if 0<=ROW<maxROW and 0<=COL<maxCOL:	
-		hdu[ROW,COL]+=mToCounts(m,20,flux20)
+	(ROW,COL) = RADECtoRowCol(RA,DEC)
+	if 0 <= ROW < maxROW and 0 <= COL < maxCOL:	
+		hdu[ROW,COL] += mToCounts(m,20,flux20)
 		#print ROW,COL,mToCounts(m,20,flux20)
 	return
 
@@ -43,32 +50,24 @@ def addStellarCatalog(hdu, catalog):
 	for linea in open(catalog):
 		linea = linea.strip()
 		obj, ra, dec,mag,sed,index,tipo = linea.split()
-		ra=float(ra)
-		dec=float(dec)
-		mag=float(mag)
+		ra = float(ra)
+		dec = float(dec)
+		mag = float(mag)
 		#print ra,dec,RADECtoRowCol(ra,dec)
 		addStar(hdu,mag,ra,dec)
 	return
-
-def psersic(Re,n,m,xc,yc,x,y,el,theta):
-	bn=2*n-0.324
-	ln=mToCounts(m,20,flux20)
-	I0=ln*bn**2/((Re**2)*2*pi*n*gamma(2*n))
-	E=sqrt(((x-xc)*cos(theta)+(y-yc)*sin(theta))**2+((x-xc)*sin(theta)-(y-yc)*cos(theta))**2/(1-el)**2)
-	I=I0*exp(-bn*(E/Re)**(1/n))
-	return (I)
 	
 def addGalaxy(hdu, m, RA, DEC, n, Re, el, theta):
 	(ROW,COL)=RADECtoRowCol(RA,DEC)
-	if 0<=ROW<maxROW and 0<=COL<maxCOL:	
+	if 0 <= ROW < maxROW and 0 <= COL < maxCOL:	
 		a1=int(ROW-4*Re)
 		b1=int(ROW+4*Re)
 		a2=int(COL-4*Re)
 		b2=int(COL+4*Re)
-		for (y) in range(a1,b1):
-			for(x) in range(a2,b2):
-				if  0<=y<4096 and 0<=x<4096:
-					hdu[y,x]+=psersic(Re,n,m,COL,ROW,x,y,el,theta)
+		for y in range(a1,b1):
+			for x in range(a2,b2):
+				if  0 <= y < 4096 and 0 <= x < 4096:
+					hdu[y,x] += psersic(Re,n,m,COL,ROW,x,y,el,theta)
 		#hdu[ROW,COL]+=mToCounts(m,20,flux20)*(2*n-0.324)**2/((Re**2)*2*pi*n*gamma(2*n))
 		#print ROW,COL, mToCounts(m,20,flux20)*(2*n-0.324)**2/((Re**2)*2*pi*n*gamma(2*n))
 	return
@@ -78,16 +77,16 @@ def addGalaxyCatalog(hdu, catalog):
 	for linea in open(catalog):
 		linea = linea.strip()
 		obj, ra, dec,mag,sed,redshift,tipo,n,re,elip,o = linea.split()
-		ra=float(ra)
-		dec=float(dec)
-		mag=float(mag)
-		n=float(n)
-		re=float(re)
-		elip=float(elip)
-		o=float(o)
+		ra = float(ra)
+		dec = float(dec)
+		mag = float(mag)
+		n = float(n)
+		re = float(re)
+		elip = float(elip)
+		o = float(o)
 		addGalaxy(hdu,mag,ra,dec,n,re,elip,o)
-		i+=1
-		if i>100000: break
+		i += 1
+		if i > 100000: break
 	return
 
 def addBackground (hdu, background):
@@ -108,8 +107,9 @@ def filterImage (hdu, params):
     return
 
 if __name__=="__main__":
+	#test de RADEC
 	ra = 22.1142135471
 	dec = -0.112283531445
-	print "RA: "+ str(ra) +"\t\t\tDEC: "+str(dec)
+	print "RA: " + str(ra) + "\t\t\tDEC: " + str(dec)
 	(x,y) = RADECtoRowCol(ra,dec)
-	print "X: "+ str(x) +"\t\t\tY: "+str(y)
+	print "X: " + str(x) + "\t\t\tY: " + str(y)
