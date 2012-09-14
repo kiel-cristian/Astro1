e948df36a45ccde7eafa7d677dc6ca4721777ec1
Cristian <the.killer06@gmail.com>
2012-09-14 04:26:44 -0300

    plotear imagen

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pylab as pl
 import scipy.signal as sig
 import scipy as sp
 import numpy as np
+from fits_lib import *
 
 import metodos_t1 as m
 hdulist = pf.open('blank.fits')
@@ -13,3 +14,9 @@ catalog2="galaxy.dat"
 m.addStellarCatalog(hdu,catalog1)
 m.addGalaxyCatalog(hdu,catalog2)
 hdulist.close()
+
+hdr1 = get_fits_header('blank.fits')
+img1 = get_fits_matrix('blank.fits')
+print "\n>> ARCHIVO 1:\n"
+print_header(hdr1)
+plot_image(img1)
