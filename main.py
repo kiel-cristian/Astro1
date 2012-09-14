e1d4f6af04c16cc4c2bdd5de0109618ad0c2e185
Cristian <the.killer06@gmail.com>
2012-09-14 05:01:31 -0300

    background

--- main.py
+++ main.py
@@ -4,15 +4,18 @@ import scipy.signal as sig
 import scipy as sp
 import numpy as np
 from fits_lib import *
+from metodos_t1 import *
 
-import metodos_t1 as m
 hdulist = pf.open('blank.fits')
 hdu= hdulist[0].data
 catalog1="stellar.dat"
 catalog2="galaxy.dat"
+background = 0
 
-m.addStellarCatalog(hdu,catalog1)
-m.addGalaxyCatalog(hdu,catalog2)
+# addStellarCatalog(hdu,catalog1)
+# addGalaxyCatalog(hdu,catalog2)
+# addBackground(hdu,background)
+
 
 plot_image(hdu,log_scale=True)
 hdulist.close()
